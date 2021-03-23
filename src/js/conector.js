eb828c3e39bd580b11cbe09a70e3dc69d165deb7
Fede Davila <federicordavila@gmail.com>
2021-03-23 16:05:54 -0300

    prueba 4 v8

--- src/js/conector.js
+++ src/js/conector.js
@@ -21,10 +21,8 @@ window.TrelloPowerUp.initialize({
       url: 'https://developer.atlassian.com/cloud/trello',
       target: 'Trello Developer Site' // optional target for above url
     }];
-  }
-});
-/*
-window.TrelloPowerUp.initialize({
+  },
+  
     'card-badges': function(t, opts) {
         // return an array of card budges for the given card
         return t.card('all')
@@ -35,28 +33,5 @@ window.TrelloPowerUp.initialize({
                     text: card.idShort
                 }];
             });
-    },
-
-    'card-buttons': function (t, opts) {
-        return t.card('idShort')
-            .then(function(card) {
-                //console.log('Presionó el botón');
-
-                return [{
-                    // usually you will provide a callback function to be run on button click
-                    // we recommend that you use a popup on click generally
-                    icon: GRAY_ICON, // don't use a colored icon here
-                    text: 'Open Popup',
-                    callback: onBtnClick,
-                    condition: 'edit'
-                }, {
-                    // but of course, you could also just kick off to a url if that's your thing
-                    icon: GRAY_ICON, // don't use a colored icon here
-                    text: 'GoGoGo',
-                    condition: 'always',
-                    url: 'http://www.multiled.com.ar/',
-                    target: 'Multiled SA' // optional target for above url
-                }];
-        });
-      }
-});*/
+    }
+});
