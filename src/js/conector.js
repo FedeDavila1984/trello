eb3f514e16bd7870b160d8d22db2d6a206b07fe4
Fede Davila <federicordavila@gmail.com>
2021-03-23 15:24:12 -0300

    prueba 4 v5

--- src/js/conector.js
+++ src/js/conector.js
@@ -26,16 +26,16 @@ window.TrelloPowerUp.initialize({
                     // usually you will provide a callback function to be run on button click
                     // we recommend that you use a popup on click generally
                     icon: GRAY_ICON, // don't use a colored icon here
-                    text: 'GoGoGo',
-                    condition: 'always',
-                    url: 'http://www.multiled.com.ar/' + card.idShort + '"',
-                    target: 'Multiled SA' // optional target for above url
-                }, {
-                    // but of course, you could also just kick off to a url if that's your thing
-                    icon: GRAY_ICON, // don't use a colored icon here
                     text: 'Open Popup',
                     callback: onBtnClick,
                     condition: 'edit'
+                }, {
+                    // but of course, you could also just kick off to a url if that's your thing
+                    icon: GRAY_ICON, // don't use a colored icon here
+                    text: 'GoGoGo',
+                    condition: 'always',
+                    url: 'http://www.multiled.com.ar/',
+                    target: 'Multiled SA' // optional target for above url
                 }];
         //});
       }
