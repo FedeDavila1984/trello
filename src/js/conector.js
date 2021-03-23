133c58e4106363431a3e885c12af776c7dd96366
Fede Davila <federicordavila@gmail.com>
2021-03-23 14:53:22 -0300

    prueba 2

--- src/js/conector.js
+++ src/js/conector.js
@@ -2,6 +2,13 @@ console.log('HELOOOOOOUUUUU');
 window.TrelloPowerUp.initialize({
     'card-budges': function(t, opts) {
         // return an array of card budges for the given card
-        return [];
+        return t.card('all')
+            .then(function(card) {
+                console.log(card);
+
+                return [{
+                    text: card.idShort
+                }];
+            });
     }
 });
