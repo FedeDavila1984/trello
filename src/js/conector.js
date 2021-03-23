fbbc2fe0741f97d51cda4d799e483b27d3620373
Fede Davila <federicordavila@gmail.com>
2021-03-23 15:13:16 -0300

    prueba 4

--- src/js/conector.js
+++ src/js/conector.js
@@ -7,8 +7,31 @@ window.TrelloPowerUp.initialize({
                 console.log(card);
 
                 return [{
-                    text: '<a href="http://www.multiled.com.ar/' + card.idShort + '" target="_blank">' + card.idShort + ' vence: ' + card.due + '</a>'
+                    text: card.idShort
                 }];
             });
-    }
+    },
+
+    'card-buttons': function (t, opts) {
+        return t.card('idShort')
+            .then(function(card) {
+                console.log('Presionó el botón');
+
+                return [{
+                    // usually you will provide a callback function to be run on button click
+                    // we recommend that you use a popup on click generally
+                    icon: GRAY_ICON, // don't use a colored icon here
+                    text: 'Open Popup',
+                    callback: onBtnClick,
+                    condition: 'edit'
+                }, {
+                    // but of course, you could also just kick off to a url if that's your thing
+                    icon: GRAY_ICON,
+                    text: 'Just a URL',
+                    condition: 'always',
+                    url: 'http://www.multiled.com.ar/' + card.idShort + '"',
+                    target: 'Trello Developer Site' // optional target for above url
+                }];
+        });
+      }
 });
