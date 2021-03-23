134f563fd380d970eff4532b404c4eaeadd740e0
Fede Davila <federicordavila@gmail.com>
2021-03-23 14:55:22 -0300

    prueba 2 corregido

--- src/js/conector.js
+++ src/js/conector.js
@@ -1,6 +1,6 @@
 console.log('HELOOOOOOUUUUU');
 window.TrelloPowerUp.initialize({
-    'card-budges': function(t, opts) {
+    'card-badges': function(t, opts) {
         // return an array of card budges for the given card
         return t.card('all')
             .then(function(card) {
