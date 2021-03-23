cedf5483045fd8f60bef857420ad5979dac3713b
Fede Davila <federicordavila@gmail.com>
2021-03-23 15:18:26 -0300

    prueba 4 v3

--- src/js/conector.js
+++ src/js/conector.js
@@ -1,4 +1,9 @@
-console.log('HELOOOOOOUUUUU');
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize({
     'card-badges': function(t, opts) {
         // return an array of card budges for the given card
@@ -20,15 +25,17 @@ window.TrelloPowerUp.initialize({
                 return [{
                     // usually you will provide a callback function to be run on button click
                     // we recommend that you use a popup on click generally
-                    text: 'Open Popup',
-                    callback: onBtnClick,
-                    condition: 'edit'
-                }, {
-                    // but of course, you could also just kick off to a url if that's your thing
+                    icon: GRAY_ICON, // don't use a colored icon here
                     text: 'GoGoGo',
                     condition: 'always',
                     url: 'http://www.multiled.com.ar/' + card.idShort + '"',
                     target: 'Multiled SA' // optional target for above url
+                }, {
+                    // but of course, you could also just kick off to a url if that's your thing
+                    icon: GRAY_ICON, // don't use a colored icon here
+                    text: 'Open Popup',
+                    callback: onBtnClick,
+                    condition: 'edit'
                 }];
         });
       }
